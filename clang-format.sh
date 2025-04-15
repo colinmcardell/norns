73018c609aaca472d23b9ce3dbb290447365a907
Colin McArdell <colin@colinmcardell.com>
2025-04-15 02:16:29 +0000

    fix: :bug: Ensure clang-format runs on crone/src directory for consistent code formatting

--- clang-format.sh
+++ clang-format.sh
@@ -2,3 +2,4 @@
 # run this from top-level norns directory before committing...
 find ws-wrapper/src -regex .*[\.][ch] | xargs clang-format -i
 find matron/src -regex .*[\.][ch] | xargs clang-format -i
+find crone/src -regex .*[\.][ch] | xargs clang-format -i
